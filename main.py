c4dcd0a0ea65c9513be91f57b9f98d7cf017f143
Eugenio <lugezz@gmail.com>
2022-11-29 10:50:51 -0300

    Minor changes

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello World my old friend - Cachulengo"}
+
+
+@app.get("/posts")
+def get_post():
+    return {"Data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_post(pay_load: dict = Body):
+    print(pay_load)
+    return {'new_post': f"Title: {pay_load['title']}, Content: {pay_load['content']}"}
