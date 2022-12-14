0e42650fa57a6c96ad2aa1f6741d2970676fc4ae
Eugenio <lugezz@gmail.com>
2022-12-14 09:49:35 -0300

    Alembic config

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app.models import Base
 from app.routers import auth, post, user, vote
 
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(post.router)
