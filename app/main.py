1ed37190837e87197238d98d97c6e8574e4bcdca
Eugenio <lugezz@gmail.com>
2022-12-21 06:50:56 -0300

    Docker compose production

--- app/main.py
+++ app/main.py
@@ -30,4 +30,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World my old friend - Change in prod"}
+    return {"message": "Hello World my old friend - Change in Docker!!!"}
