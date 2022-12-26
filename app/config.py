4257196235c28b5811301e9e917b31900d9c386b
Eugenio <lugezz@gmail.com>
2022-12-26 13:21:46 -0300

    Python 3.10

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@ class Settings(BaseSettings):
     db_server: str
     db_port: str
     db_user: str
-    db_password: str
     db_name: str
+    db_password: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
