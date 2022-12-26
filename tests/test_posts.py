46768d9d81e517746a2baa0a2d49070107bd3914
Eugenio <lugezz@gmail.com>
2022-12-26 06:51:21 -0300

    Minor fixes

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -14,7 +14,7 @@ def test_get_all_posts(authorized_client, test_posts):
     assert res.status_code == 200
 
 
-def test_unauthorized_user_get_all_posts(client, test_posts):
+def test_unauthorized_user_get_all_posts(client):
     res = client.get("/posts")
     assert res.status_code == 401
 
@@ -24,7 +24,7 @@ def test_unauthorized_user_get_one_post(client, test_posts):
     assert res.status_code == 401
 
 
-def test_get_one_post_not_exist(authorized_client, test_posts):
+def test_get_one_post_not_exist(authorized_client):
     res = authorized_client.get("/posts/88888")
     assert res.status_code == 404
 
@@ -78,26 +78,26 @@ def test_create_post_default_published_true(authorized_client, test_user):
     assert created_post.owner_id == test_user['id']
 
 
-def test_unauthorized_user_create_post(client, test_user, test_posts):
+def test_unauthorized_user_create_post(client):
     res = client.post(
         "/posts", json={"title": "arbitrary title", "content": "aasdfjasdf"})
     assert res.status_code == 401
 
 
-def test_unauthorized_user_delete_Post(client, test_user, test_posts):
+def test_unauthorized_user_delete_Post(client, test_posts):
     res = client.delete(
         f"/posts/{test_posts[0].id}")
     assert res.status_code == 401
 
 
-def test_delete_post_success(authorized_client, test_user, test_posts):
+def test_delete_post_success(authorized_client, test_posts):
     res = authorized_client.delete(
         f"/posts/{test_posts[0].id}")
 
     assert res.status_code == 204
 
 
-def test_delete_post_non_exist(authorized_client, test_user, test_posts):
+def test_delete_post_non_exist(authorized_client):
     res = authorized_client.delete(
         "/posts/8000000")
 
@@ -124,7 +124,7 @@ def test_update_post(authorized_client, test_posts):
     assert updated_post.content == data['content']
 
 
-def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
+def test_update_other_user_post(authorized_client, test_posts):
     data = {
         "title": "updated title",
         "content": "updatd content",
@@ -135,13 +135,13 @@ def test_update_other_user_post(authorized_client, test_user, test_user2, test_p
     assert res.status_code == 403
 
 
-def test_unauthorized_user_update_post(client, test_user, test_posts):
+def test_unauthorized_user_update_post(client, test_posts):
     res = client.put(
         f"/posts/{test_posts[0].id}")
     assert res.status_code == 401
 
 
-def test_update_post_non_exist(authorized_client, test_user, test_posts):
+def test_update_post_non_exist(authorized_client, test_posts):
     data = {
         "title": "updated title",
         "content": "updatd content",
